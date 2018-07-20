f6d3a359907e1c670d510aa7fc670ea5e870c42f
erdii <erdiicodes@gmail.com>
2018-07-20 22:19:11 +0200

    add commonjs module resolution - import like esmodules

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import { uglify } from "rollup-plugin-uglify";
 import htmlTemplate from "rollup-plugin-generate-html-template";
 import browsersync from "rollup-plugin-browsersync";
 import replace from "rollup-plugin-replace";
+import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from "rollup-plugin-node-resolve";
 
 const packageJSON = require("./package.json");
 
@@ -22,6 +24,39 @@ const options = {
 };
 
 const defaultPlugins = [
+	nodeResolve({
+		jsnext: true,
+		main: true
+	}),
+	commonjs({
+		// non-CommonJS modules will be ignored, but you can also
+		// specifically include/exclude files
+		include: 'node_modules/**',  // Default: undefined
+		exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
+		// these values can also be regular expressions
+		// include: /node_modules/
+
+		// search for files other than .js files (must already
+		// be transpiled by a previous plugin!)
+		extensions: [ '.js' ],  // Default: [ '.js' ]
+
+		// if true then uses of `global` won't be dealt with by this plugin
+		ignoreGlobal: false,  // Default: false
+
+		// if false then skip sourceMap generation for CommonJS modules
+		sourceMap: false,  // Default: true
+
+		// explicitly specify unresolvable named exports
+		// (see below for more details)
+		// namedExports: { './module.js': ['foo', 'bar' ] },  // Default: undefined
+
+		// sometimes you have to leave require statements
+		// unconverted. Pass an array containing the IDs
+		// or a `id => boolean` function. Only use this
+		// option if you know what you're doing!
+		// ignore: [ 'conditional-runtime-dependency' ]
+	}),
+
 	typescript({
 		typescript: require("typescript"),
 	}),
